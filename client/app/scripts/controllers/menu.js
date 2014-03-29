d82d8e80e65458349c3661395ca484c5738e76e0
Riku Avelar <riku.avelar@gmail.com>
2014-03-29 14:49:00 -0400

    Added More stuff for menu

--- client/app/scripts/controllers/menu.js
+++ client/app/scripts/controllers/menu.js
@@ -1,8 +1,59 @@
 'use strict';
 
 angular.module('clientApp')
-  .controller('MenuCtrl', function ($scope, Menu) {
-    Menu.getOwn({}, function(menus){
-      $scope.menus = _.groupBy(menus, 'restaurant');
-    });
+  .controller('MenuCtrl', function ($scope, Menu, $routeParams, $location) {
+    $scope.currentMenu = {};
+    $scope.currentRepas = {};
+
+    if(!$routeParams.id) {
+      Menu.getOwn({}, function(menus){
+        $scope.menus = _.groupBy(menus, 'restaurant');
+      });
+    } else {
+      Menu.get({id: $routeParams.id}, function(menu){
+        $scope.currentMenu = menu;
+      });
+    }
+
+    var filterFloat = function (value) {
+        if(/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/
+          .test(value))
+          return Number(value);
+      return NaN;
+    }
+
+    var addRepas = function(nouveauRepas){
+      var repas = _.clone(nouveauRepas);
+      $scope.currentMenu.repas = $scope.currentMenu.repas || [];
+      $scope.currentMenu.repas.push(repas);
+    };
+
+    var modifyRepas = function(existing, nouveauRepas){
+      _.extend(existing, nouveauRepas);
+    };
+
+    $scope.addOrModifyRepas = function(repas){
+      var existingEntry = _.findWhere($scope.currentMenu.repas, {nom: repas.nom});
+      if(existingEntry) {
+        modifyRepas(existingEntry, repas);
+      } else {
+        addRepas(repas);
+      }
+    };
+
+    $scope.emptyMenu = function(){
+      return !$scope.currentMenu.repas || ($scope.currentMenu.repas && !$scope.currentMenu.repas.length)
+    };
+
+    $scope.setCurrentRepas = function(repas){
+      $scope.currentRepas = _.clone(repas);
+      $scope.currentRepas.prix = filterFloat(repas.prix);
+    };
+
+    $scope.confirmMenu = function(){
+      $scope.currentMenu.$save().$promise.then(function(){
+        $location.path('/menus')
+      });
+    };
+
   });
