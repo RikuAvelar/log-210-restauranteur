7e6926e265372d040361f91f08f161922afc16bd
Riku Avelar <riku.avelar@gmail.com>
2014-03-31 00:35:17 -0400

    Basic Restaurateur list attached

--- client/app/scripts/controllers/restaurateurs.js
+++ client/app/scripts/controllers/restaurateurs.js
@@ -1,12 +1,25 @@
 'use strict';
 
 angular.module('clientApp')
-  .controller('RestaurateursCtrl', function ($scope, $routeParams) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.params = $routeParams;
-    //$scope.reverseAnimation = true;
+  .controller('RestaurateursCtrl', function ($scope, $routeParams, Restaurateur, AddressFormatter) {
+    if (!$routeParams.id) {
+      //No id => Restaurateur list
+      $scope.restaurateurs = Restaurateur.all();
+    } else {
+      if($routeParams.id == 'new') {
+        $scope.currentRestaurateur = new Restaurateur();
+      } else {
+        $scope.currentRestaurateur = Restaurateur.get({id: $routeParams.id});
+      }
+    }
+
+    $scope.address = AddressFormatter.address;
+    $scope.restaurantList = function(restaurateur) {
+      var restaurants = restaurateur.restaurants;
+
+      if(_.isEmpty(restaurants)) {
+        restaurants = [{name: '( No restaurants assigned )'}];
+      }
+      return _.pluck(restaurants, 'name').join(', ');
+    }
   });
--- client/app/scripts/services/restaurateur.js
+++ client/app/scripts/services/restaurateur.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('clientApp')
-  .factory('Restaurant', function ($resource, AuthService) {
-    var restaurant = $resource('/api/restaurants/:id', {id: '@id'}, {
+  .factory('Restaurateur', function ($resource, AuthService) {
+    var restaurateur = $resource('/api/restaurateurs/:id', {id: '@id'}, {
       'get': 'GET',
       'all': {method: 'GET', isArray: true},
       'put': {method: 'PUT'},
       '$update': {method: 'PUT'}
     });
 
-    return restaurant;
+    return restaurateur;
   });
 
 /**
